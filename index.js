1336981ae10a3dd3d2620a1c614da9c2220f0089
Suvarna Jadhav <jadhav.suvarna28@gmail.com>
2023-01-24 13:18:48 -0500

    added class files and created foldr structure as mentioned in the readme file

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for my team members and their information
-// THEN an HTML file is generated that displays a nicely formatted team roster based on user input
-// WHEN I click on an email address in the HTML
-// THEN my default email program opens and populates the TO field of the email with the address
-// WHEN I click on the GitHub username
-// THEN that GitHub profile opens in a new tab
-// WHEN I start the application
-// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
-// WHEN I enter the team manager’s name, employee ID, email address, and office number
-// THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
-// WHEN I select the engineer option
-// THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
-// WHEN I select the intern option
-// THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
-// WHEN I decide to finish building my team
-// THEN I exit the application, and the HTML is generated
-
 const inquirer = require('inquirer');
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+const fs = require('fs');
 
 // Function that uses inquirer package's prompt method to ask a series of questions to the users to create the README file
-function init() { 
+function init() {
     inquirer.prompt([
         {
             type: 'input',
@@ -48,10 +34,11 @@ function init() {
             choices: ['Add an engineer', 'Add an intern', 'Finish building your team'],
         },
     ])
-    .then((data) => {
-        console.log('Team built successfully!')
-    });
-}
+        .then((data) => {
+            const manager = new Manager(data.managername, data.employeeID, data.email, data.officenumber)
+            console.log(manager);
+        })
+    }
 
 // Function call that initializes the app
 init();
